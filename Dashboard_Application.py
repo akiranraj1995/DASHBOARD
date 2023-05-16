bcfd02a0029e01e55ffa3db34438a96efa192ea1
A Kiran Raj <ambal.kiranraj@gmail.com>
2023-05-16 14:12:15 +0530

    Update Dashboard_Application.py

--- Dashboard_Application.py
+++ Dashboard_Application.py
@@ -114,7 +114,7 @@ def display_summary_statistics(df):
 
     # Set the "No" column as the index
     mac_stats = mac_stats.set_index("No")
-    st.write(mac_stats)
+    st.write(mac_stats, width=1000)
 
 
 total_records1 = 0
