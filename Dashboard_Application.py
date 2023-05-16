25f0517bef9ba6abc3fc1bf227feaebfaa489ac9
A Kiran Raj <ambal.kiranraj@gmail.com>
2023-05-16 17:14:56 +0530

    Update Dashboard_Application.py

--- Dashboard_Application.py
+++ Dashboard_Application.py
@@ -107,6 +107,10 @@ def display_summary_statistics(df):
     # format the other columns to display without decimal places
     mac_stats = mac_stats.applymap(lambda x: "{:.0f}".format(x) if isinstance(x, (int, float)) else x)
     
+    # Set the "No" column as the index and rename the index column
+    mac_stats = mac_stats.set_index("No").rename_axis("No")
+
+    
     # Display the updated results
     st.subheader(f"1. Total Records: {int(total_records)}")
    
