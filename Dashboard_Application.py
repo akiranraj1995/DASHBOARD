26ea49fab3687be08965d43b4b70c47dd6ceddb4
A Kiran Raj <ambal.kiranraj@gmail.com>
2023-05-17 22:01:27 +0530

    Update Dashboard_Application.py

--- Dashboard_Application.py
+++ Dashboard_Application.py
@@ -26,7 +26,7 @@ st.set_page_config(**default_config)
 
 # Define constants for Google Drive API
 SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
-SERVICE_ACCOUNT_FILE = 'fresh-deck-324409-5a5c7482c3d0.json'
+SERVICE_ACCOUNT_FILE = '/home/expert/Jupyter Notebook/MAY2023/Agverse_9th_Week(15May2023-20thMay2023)/fresh-deck-324409-5a5c7482c3d0.json'
 
 st.title("DATA DASHBOARD & LIVE LOCATIONS")
 # Create a Streamlit container to display the results
